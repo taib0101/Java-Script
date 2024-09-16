be6ea638cb59966e2c8d2a7e9ecd46647517d75d
taib0101 <taibvai13@gmail.com>
2024-09-17 01:34:20 +0600

    added

---
+++ this keyword/new_rules.js
@@ -0,0 +1,9 @@
+let Constructor = function(name,age) {
+    this.name = name;
+    this.age = age;
+};
+
+let object = new Constructor("Taib",26);
+console.log(object);
+console.log(object.name);
+console.log(object.age);
