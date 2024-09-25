ea8c2b5cdad29566bf9e30ba482a65f20eaf2ee8
Mustain Murtaza Taib <65018690+taib0101@users.noreply.github.com>
2024-09-26 01:56:40 +0600

    updated

--- Loop/forInOF.js
+++ Loop/forInOF.js
@@ -8,3 +8,18 @@ for(let index in arr) {
 for(let value of arr) {
   console.log(value);
 }
+
+
+let object = {
+    name : "Taib",
+    age : 30
+};
+
+console.log();
+for(let key in object) {
+    console.log(key);
+}
+
+for(let [key,value] of Object.entries(object)) {
+    console.log(key,value);
+}
