03c84bda4388acf4bc004b4dd3baf22de24802be
Mustain Murtaza Taib <65018690+taib0101@users.noreply.github.com>
2024-11-13 03:35:45 +0600

    updated

--- Date/date_time.js
+++ Date/date_time.js
@@ -3,12 +3,16 @@ const dateWithUnReadable = Date.now();
 console.log(dateWithUnReadable);
 
 // Date with human Readable
-const dateWithReadable = new Date(Date.now());
+// const dateWithReadable = new Date(Date.now());
+// after 1 hour time and date
+const dateWithReadable = new Date(Date.now() + (1000 * 60 * 60));
 console.log(dateWithReadable);
 
 // Human Readable Locale Date And Time
 // const localeDateAndTime = new Date(Date.now()).toLocaleString();
-const localeDateAndTime = new Date(Date.now()).toLocaleString("en-US", { timeZone: "Asia/Dhaka" });
+// const localeDateAndTime = new Date(Date.now()).toLocaleString("en-US", { timeZone: "Asia/Dhaka" });
+// after 1 hour time and date
+const dateWithReadable = new Date(Date.now() + (1000 * 60 * 60)).toLocaleString("en-US", { timeZone: "Asia/Dhaka" });
 
 // Human Readable Locale Date 
 // const localeDate = new Date(Date.now()).toLocaleDateString();
@@ -18,3 +22,5 @@ console.log(localeDate);
 // Human Readable Locale Time
 const localeTime = new Date(Date.now()).toLocaleTimeString();
 console.log(localeTime);
+
+// but JWT take expire time in differently in JSON payload , goto ExpressJs/JSON Web Token/expire.js
